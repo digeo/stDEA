26b3c5dca8138b02762027feea222f87476f7d67
digeo <ma-los-sis@hotmail.com>
2015-05-01 21:16:53 +0300

    eff.dea, eff.stdea, technical.eff.dea, technical.eff.stdea, are calculated except: slacks, proj.val

--- R/stDEA.R
+++ R/stDEA.R
@@ -37,8 +37,6 @@ stDEA <- function(X, Y, RTS = "vrs", ORIENTATION = "out", stp = 0.01){
   if (!(ORIENTATION %in% orientation)) {
     stop(paste("Unknown value for ORIENTATION:", ORIENTATION))
   }
-    
-  repeats <- 1 / stp
   
   numberOfDMUs <- dim(X)[1]
   numberOfOutputs <- dim(Y)[2]
@@ -64,39 +62,68 @@ stDEA <- function(X, Y, RTS = "vrs", ORIENTATION = "out", stp = 0.01){
   # Define the rhs
   rightHandSide <- matrix(data = 0, nrow = numberOfOutputs, ncol = 1)
   
-  # ST-DEA
-  for (i in minimumDMUs:numberOfDMUs){
-    objectiveFunction <- matrix(data = 0, nrow = i, ncol = 1)
-    
-    
-    for(Wsp in seq(0, 1, stp)){
-      Wt <- 1 - Wsp
-      
-      lpmodel <- make.lp((numberOfOutputs + 2), (i + 1))
-      
-      # The 1st column, is the column for the phi
-      set.column(lpmodel, 1, c((-Y[i,]), 1, 0))
-      
-      for (j in 1:i){
-        objectiveFunction[j] <- (((Wsp / alphaMax[i]) * alphaMatrix[i, j]) - ((Wt / deltaMin[i]) * deltaMatrix[i, j]))
-        set.column(lpmodel, (j + 1), c((Y[j,]), 0, 1))
-      }
-      
-      set.objfn(lpmodel, c(0, objectiveFunction[1:i]))
-      set.constr.type(lpmodel, c(constraintTypes[1:(numberOfOutputs + 1)], "="))
-      set.rhs(lpmodel, c(rightHandSide[1:numberOfOutputs], 1, 1))
-      
-      set.type(lpmodel, 2:(i + 1), "binary")
-      
-      # Set sense for the Linear Problem
-      lp.control(lpmodel, sense = "max")
-      
-      solve(lpmodel)
-    }
-  }
+  st.peer <- NULL
+  #technical.efficiency <- matrix(data = NA, nrow = (1 / 0.01 + 1), ncol = numberOfOutputs)
+  eff.dea <- eff(e)[numberOfDMUs]
+  eff.stdea <- NULL
+  technical.eff.dea <- 1 / eff.dea
+  technical.eff.stdea <- NULL
+  slacks <- matrix(data = NA, nrow = (1 / 0.01 + 1), ncol = numberOfOutputs)
+  proj.val <- matrix(data = NA, nrow = (1 / 0.01 + 1), ncol = numberOfOutputs)
   
-  oe <- list(eff = e,
-             efficiencyDEA = efficiency.dea,
+  # ST-DEA
+  objectiveFunction <- matrix(data = 0, nrow = i, ncol = 1)
+  for(Wsp in seq(0, 1, stp)){
+    Wt <- 1 - Wsp
+    
+    lpmodel <- make.lp((numberOfOutputs + 2), (i + 1))
+    
+    # The 1st column, is the column for the phi
+    set.column(lpmodel, 1, c((-Y[i,]), 1, 0))
+    
+    for (j in 1:i){
+      objectiveFunction[j] <- (((Wsp / alphaMax[i]) * alphaMatrix[i, j]) - ((Wt / deltaMin[i]) * deltaMatrix[i, j]))
+      set.column(lpmodel, (j + 1), c((Y[j,]), 0, 1))
+    }
+    
+    set.objfn(lpmodel, c(0, objectiveFunction[1:i]))
+    set.constr.type(lpmodel, c(constraintTypes[1:(numberOfOutputs + 1)], "="))
+    set.rhs(lpmodel, c(rightHandSide[1:numberOfOutputs], 1, 1))
+    
+    set.type(lpmodel, 2:(i + 1), "binary")
+    
+    # Set sense for the Linear Problem
+    lp.control(lpmodel, sense = "max")
+    
+    solve(lpmodel)
+    
+    st.peer <- c(st.peer, which.max(get.variables(lpmodel)[2:i]))
+    
+    row.Number <- Wsp * 100 + 1
+    for(ii in 1:numberOfOutputs){
+      technical.efficiency[row.Number, ii] <- Y[(which.max(get.variables(lpmodel)[2:i])), ii] / Y[i, ii]
+    }
+    
+    eff.stdea <- c(eff.stdea, min(technical.efficiency[row.Number,]))
+    
+    technical.eff.stdea <- c(technical.eff.stdea, 1 / eff.stdea[row.Number])
+    
+    for(ii in 1:numberOfOutputs){
+      slacks[row.Number, ii] <- Y[(which.max(get.variables(lpmodel)[2:numberOfDMUs])), ii] - eff.stdea[row.Number]  * Y[numberOfDMUs, ii]
+      proj.val[row.Number, ii] <- eff.stdea[numberOfDMUs] * Y[numberOfDMUs, ii]
+    }
+    
+    rm(lpmodel)
+  }
+  View(eff.dea)
+  View(eff.stdea)
+  View(technical.eff.dea)
+  View(technical.eff.stdea)
+  View(slacks)
+  View(proj.val)
+  
+  oe <- list(effDEA = eff.dea,
+             effstDEA = eff.stdea,
              lambda = lambda,
              objval = objval,
              RTS = RTS,
